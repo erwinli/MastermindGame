19899cd44eda525a9dbe73412714adc8569c313a
Tritonia <Tritonia@Erwins-MacBook-Pro.local>
2014-09-26 11:45:02 -0700

    Initial Commit

---
+++ MastermindGame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MastermindGame
+//
+//  Created by Tritonia on 2014-09-26.
+//  Copyright (c) 2014 Tritonia. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
